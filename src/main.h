556f74dc8cbe1bc6f040139029cf870562b918f0
oldmagic <oldmagic@zenet.org>
2017-08-20 13:17:29 +0200

    Update main.h
    Changed total coins supply to 198800000 (1988 was the year IRC was created)

--- src/main.h
+++ src/main.h
@@ -54,7 +54,7 @@ static const int64 DUST_SOFT_LIMIT = 100000; // 0.001 BRD
 /** Dust Hard Limit, ignored as wallet inputs (mininput default) */
 static const int64 DUST_HARD_LIMIT = 1000;   // 0.00001 BRD mininput
 /** No amount larger than this (in satoshi) is valid */
-static const int64 MAX_MONEY = 55000000000 * COIN; // 55 bil
+static const int64 MAX_MONEY = 198800000 * COIN; // 55 bil
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 50;
